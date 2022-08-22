ebd8865644e4d32e9307b820519da81e81204b07
Cno <dongheming@bytedance.com>
2022-08-22 19:42:22 +0800

    增加home栅格布局

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,16 @@
+import "styles/home.scss"
+import {SmileOutlined} from "@ant-design/icons";
+
 export const Home = () => {
   return (
-    <h1>Hello Home</h1>
+    <div className="home__bg-container">
+      <h2 className="home__welcome">🛏夜深了，卡诺记得早睡早起哦！</h2>
+      <div className="home__status">
+        <SmileOutlined/>
+        <p>您已拥有最新版本的 Edgeless Beta</p>
+      </div>
+      <div className="home__plugins-management">已安装1个插件</div>
+      <div className="home__plugins-recommendation">随便看看</div>
+    </div>
   );
 };

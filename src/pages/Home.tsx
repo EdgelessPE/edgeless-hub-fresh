0cb2dc2868f2d4382c3269a9fec93f366a8b58fe
Cno <dongheming@bytedance.com>
2022-08-23 15:18:14 +0800

    完成不完全体的home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,85 @@
 import "styles/home.scss"
-import {SmileOutlined} from "@ant-design/icons";
+import {SmileTwoTone} from "@ant-design/icons";
+import {Alert, Avatar, Button, Divider, List, Space, Tag} from "@arco-design/web-react";
+import {IconRefresh} from "@arco-design/web-react/icon";
+import {useState} from "react";
+import {myHistory} from "@/router/history";
+
+const pluginsRecommendation = new Array(4).fill({
+  title: '腾讯会议',
+  description: '1.1.4.0 By Cno（Bot）',
+  link: '/settings'
+});
 
 export const Home = () => {
+  const [displayNotice, setDisplayNotice] = useState(true)
   return (
-    <div className="home__bg-container">
-      <h2 className="home__welcome">🛏夜深了，卡诺记得早睡早起哦！</h2>
-      <div className="home__status">
-        <SmileOutlined/>
-        <p>您已拥有最新版本的 Edgeless Beta</p>
+    <div>
+      {displayNotice && (
+        <Space direction='vertical' className="home__notice-container">
+          <Alert
+            type='info'
+            title='请尽快升级至新版本 Edgeless Hub'
+            content='1.1.4 版本修复了 Ventoy 启动盘制作后提升找不到盘符的问题'
+            closable
+            closeElement={<a>我知道了</a>}
+          />
+          <Alert
+            type='info'
+            content='1.1.4 版本现已可用👌🏻'
+            closable
+            closeElement={<a>立即更新</a>}
+          />
+        </Space>
+      )}
+
+      <div className="home__bg-container">
+        <h2 className="home__welcome">🛏夜深了，卡诺记得早睡早起哦！</h2>
+        <div className="home__status">
+          <Avatar size={128} className="home__status__avatar">
+            <SmileTwoTone/>
+          </Avatar>
+          <h2 className="home__status__h2">您已拥有最新版本的 Edgeless Beta</h2>
+          <Space>
+            <Tag bordered color="green">Beta</Tag>
+            <Tag bordered color="blue">4.1.0</Tag>
+          </Space>
+          <p>整点新鲜插件？</p>
+          <div className="home__status__button">
+            <Button>随便看看</Button>
+          </div>
+
+        </div>
+        <div className="home__plugins-management">
+          已安装1个插件
+          <Button>管理</Button>
+        </div>
+        <div className="home__plugins-recommendation">
+          <Divider style={{marginTop: "0"}}/>
+          随便看看
+          <List
+            wrapperClassName="home__plugins-recommendation__list"
+            bordered={false}
+            size="large"
+            dataSource={pluginsRecommendation}
+            render={(item, index) => (
+              <List.Item
+                key={index}
+                actions={[<Button onClick={() => myHistory.push(item.link)}>查看</Button>]}
+              >
+                <List.Item.Meta
+                  title={item.title}
+                  description={item.description}
+                />
+              </List.Item>
+            )}
+          />
+          <Button type="text">
+            <IconRefresh/>
+            换一批
+          </Button>
+        </div>
       </div>
-      <div className="home__plugins-management">已安装1个插件</div>
-      <div className="home__plugins-recommendation">随便看看</div>
     </div>
   );
 };

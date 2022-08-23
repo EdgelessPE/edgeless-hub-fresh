25c1accbfb6e35a4798d451c7241956119f11d09
Cno <dongheming@bytedance.com>
2022-08-23 17:58:32 +0800

    完成设置页面ui

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,8 +1,37 @@
-import {Avatar, List, Radio} from "@arco-design/web-react";
+import {Avatar, Button, List, Radio} from "@arco-design/web-react";
 import "styles/settings.scss"
-import {SkinOutlined} from "@ant-design/icons";
-import {useState} from "react";
+import {DeliveredProcedureOutlined, SkinOutlined} from "@ant-design/icons";
+import React, {useState} from "react";
 import {getCurrentTheme, setCurrentTheme} from "@/components/Theme";
+
+interface SettingItem {
+  avatar: React.ReactElement,
+  title: string,
+  description: string,
+  actions: React.ReactElement[]
+}
+
+function useSettingItems(items: SettingItem[]): React.ReactElement[] {
+  let result: React.ReactElement[] = []
+  for (let item of items) {
+    result.push((
+      <List.Item key={item.title} actions={item.actions}>
+        <List.Item.Meta
+          avatar={
+            <Avatar className="settings__avatar">
+              {item.avatar}
+            </Avatar>
+          }
+          title={item.title}
+          description={item.description}
+          className="settings__meta"
+        />
+      </List.Item>
+    ))
+  }
+
+  return result
+}
 
 export const Settings = () => {
   const [theme, setTheme] = useState(getCurrentTheme)
@@ -16,26 +45,32 @@ export const Settings = () => {
     setTheme(val)
   }
 
+  const settingItems: SettingItem[] = [
+    {
+      avatar: <SkinOutlined/>,
+      title: "主题",
+      description: "切换白昼模式与夜间模式",
+      actions: [
+        <Radio.Group type="button" value={theme} onChange={toggleTheme}>
+          <Radio value={true}>白昼</Radio>
+          <Radio value={false}>夜间</Radio>
+        </Radio.Group>
+      ]
+    },
+    {
+      avatar: <DeliveredProcedureOutlined/>,
+      title: "缓存目录（114 MB）",
+      description: "D:\\Download\\HubCache",
+      actions: [
+        <Button>查看</Button>
+      ]
+    }
+  ]
+
   return (
     <div className="settings__container">
       <List wrapperStyle={{width: "100%"}}>
-        <List.Item key={1} actions={[
-          <Radio.Group type="button" value={theme} onChange={toggleTheme}>
-            <Radio value={true}>白昼</Radio>
-            <Radio value={false}>夜间</Radio>
-          </Radio.Group>
-        ]}>
-          <List.Item.Meta
-            avatar={
-              <Avatar className="settings__avatar">
-                <SkinOutlined/>
-              </Avatar>
-            }
-            title="主题"
-            description="切换白昼模式与夜间模式"
-            className="settings__meta"
-          />
-        </List.Item>
+        {useSettingItems(settingItems)}
       </List>
     </div>
   );

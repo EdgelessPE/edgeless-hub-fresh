988b4f98a7a2ddea178d7f3112c69420ad2a636d
Cno <dsyourshy@qq.com>
2022-09-07 00:58:42 +0800

    使用变量确定空状态文本

--- src/components/molecules/EmptyInsertUDisk.tsx
+++ src/components/molecules/EmptyInsertUDisk.tsx
@@ -4,11 +4,11 @@ import {UsbOutlined} from "@ant-design/icons";
 export const EmptyInsertUDisk = () => {
   return (
     <Empty icon={
-      <div className="empty-icon">
+      <div className="empty__icon">
         <UsbOutlined/>
       </div>
     } description={
-      <h2>插入 Edgeless 启动盘以继续</h2>
+      <p className="empty__tip">插入 Edgeless 启动盘以继续</p>
     }/>
   )
 }

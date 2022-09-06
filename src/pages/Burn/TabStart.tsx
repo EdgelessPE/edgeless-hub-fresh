1038c13da382af67351e862187596808e8e2e3b4
Cno <dsyourshy@qq.com>
2022-09-07 00:54:48 +0800

    优化开始界面按钮

--- src/pages/Burn/TabStart.tsx
+++ src/pages/Burn/TabStart.tsx
@@ -1,13 +1,19 @@
-import {Button, Message, Popconfirm, Space} from "@arco-design/web-react";
-import {IconDelete, IconFire, IconThunderbolt} from "@arco-design/web-react/icon";
+import {Dropdown, Menu, Message, Modal, Space} from "@arco-design/web-react";
+import {IconFire, IconThunderbolt} from "@arco-design/web-react/icon";
 import React from "react";
 import {TabProps} from "./class"
 
 export const TabStart = ({next}: TabProps) => {
-  const cleanCache = () => {
-    Message.info({
-      content: "下载缓存已清理",
-    });
+  const confirmCleanCache = () => {
+    Modal.confirm({
+      title: "清理缓存后需要重新下载依赖文件。如果你确实遇到了制作错误请点击“确认删除”，然后尝试重新制作。",
+      okText: "确认删除",
+      onOk: async () => {
+        Message.info({
+          content: "下载缓存已清理",
+        });
+      }
+    })
   }
   return (
     <div className="burn__tab-inner__container">
@@ -20,27 +26,24 @@ export const TabStart = ({next}: TabProps) => {
         </div>
       </Space>
 
-      <Button.Group>
-        <Button
-          type="primary"
-          size="large"
-          onClick={() => next()}
-        >
-          <IconThunderbolt/>
-          立即开始
-        </Button>
-        <Popconfirm
-          title="清理缓存后需要重新下载依赖文件。如果你确实遇到了制作错误请点击“确认删除”，然后尝试重新制作。"
-          okText="确认删除"
-          onOk={cleanCache}
-        >
-          <Button
-            type="primary"
-            size="large"
-            icon={<IconDelete/>}
-          />
-        </Popconfirm>
-      </Button.Group>
+      <Dropdown.Button
+        type="primary"
+        size="large"
+        onClick={() => next()}
+        droplist={
+          <Menu>
+            <Menu.Item key="cache" onClick={confirmCleanCache}>
+              清理缓存
+            </Menu.Item>
+            <Menu.Item key="issue">
+              遇到问题？
+            </Menu.Item>
+          </Menu>
+        }
+      >
+        <IconThunderbolt/>
+        立即开始
+      </Dropdown.Button>
     </div>
   )
 }
--- src/pages/Burn/index.tsx
+++ src/pages/Burn/index.tsx
@@ -37,7 +37,7 @@ function getNextState(current: StateInfo, allowBranch = false): StateInfo {
 }
 
 export const Burn = () => {
-  const [currentState, setCurrentState] = useState<StateInfo>({state: "WaitingForVentoy", step: 2})
+  const [currentState, setCurrentState] = useState<StateInfo>({state: "Start", step: 0})
 
   const next = (state?: State) => {
     if (state != null) {

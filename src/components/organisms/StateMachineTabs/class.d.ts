c90426d257533530b49dcd5b40061b7753c57a22
Cno <dsyourshy@qq.com>
2022-09-08 01:10:16 +0800

    未经测试的StateMachineTabs初稿

---
+++ src/components/organisms/StateMachineTabs/class.d.ts
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface StateInfo<State extends string> {
+  state: State,
+  step: number,
+  isBranch?: boolean,
+  tabContent: (props: TabProps<State>) => React.ReactElement
+}
+
+interface TabProps<State extends string> {
+  next: (state?: State) => void
+}
+
+interface StateMachineNode<State extends string> {
+  state: StateInfo<State>["state"],
+  step: StateInfo<State>["step"],
+}
+
+interface Props<State extends string> {
+  states: StateInfo<State>[],
+  steps: string[], //对应states中从1开始的step，不包含隐藏的错误tab
+  initialState: StateMachineNode<State>
+}
+
+export type {
+  StateInfo,
+  TabProps,
+  StateMachineNode,
+  Props,
+}
---
+++ src/components/organisms/StateMachineTabs/index.tsx
@@ -0,0 +1,78 @@
+import React, {useState} from "react";
+import {Props, StateInfo, StateMachineNode, TabProps} from "./class";
+import {Button, Steps} from "@arco-design/web-react";
+import {log} from "@/utils";
+
+function getNextState<State extends string>(states: StateInfo<State>[], current: StateMachineNode<State>, allowBranch = false): StateMachineNode<State> {
+  for (let i = 0; i < states.length - 2; i++) {
+    const node = states[i]
+    if (!allowBranch && node.isBranch) continue
+    if (node.state == current.state) {
+      const nextNode = states[i + 1]
+      if (nextNode.isBranch) return getNextState(states, nextNode, true)
+      else return {
+        state: nextNode.state,
+        step: nextNode.step
+      }
+    }
+  }
+  console.error(`Fatal:Shouldn't receive state ${current.state}`)
+  return {state: current.state, step: current.step}
+}
+
+export function StateMachineTabs<State extends string>({states, steps, initialState}: Props<State>) {
+  const [currentState, setCurrentState] = useState(initialState)
+
+  const next = (state?: State) => {
+    if (state != null) {
+      for (let node of states) {
+        if (node.state == state) {
+          setCurrentState({
+            state: node.state,
+            step: node.step
+          })
+          log(`Info:Switch to given state ${state}`)
+          return
+        }
+      }
+      log(`Error:Fatal:Unknown next state ${state}`)
+      return
+    }
+    setCurrentState((prev: StateMachineNode<State>) => {
+      const ns = getNextState(states, prev);
+      log(`Info:Switch to next state ${ns.state}`)
+      return ns
+    })
+  }
+
+  const tabProps: TabProps<State> = {
+    next
+  }
+  const renderTab = () => {
+    for (let node of states) {
+      if (currentState.state == node.state) {
+        return node.tabContent(tabProps)
+      }
+    }
+    return <p>未知状态{currentState.state} <Button onClick={() => next(states[0].state)}>返回</Button></p>
+  }
+
+  const thrown = currentState.state == "Thrown"
+  return (
+    <div className="burn__container">
+      <Steps current={currentState.step} className="burn__steps">
+        {steps.map((title, index) => {
+          const stepStatue = (thrown || currentState.step == 0) ? "wait" : undefined
+          return (
+            <Steps.Step title={title} status={stepStatue}/>
+          )
+        })}
+        {thrown && <Steps.Step title="错误" status="error"/>}
+      </Steps>
+
+      <div className="burn__tab-view">
+        {renderTab()}
+      </div>
+    </div>
+  )
+}

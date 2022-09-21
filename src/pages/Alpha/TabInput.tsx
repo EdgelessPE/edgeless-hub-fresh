d735af27c85df9fe9fba47a33a506dd5ef2acf11
unknown <undefined_1@outlook.com>
2022-09-21 15:51:59 +0800

    修复TabInput中FileNodeLocal类型导入路径问题

--- src/pages/Alpha/TabInput.tsx
+++ src/pages/Alpha/TabInput.tsx
@@ -9,7 +9,7 @@ import { useState } from "react";
 import { LockOutlined } from "@ant-design/icons";
 import { AlphaTabProps } from "@/pages/Alpha/class";
 import { IconCloseCircle } from "@arco-design/web-react/icon";
-import { FileNodeLocal } from "@/classes/local";
+ import { FileNodeLocal } from "types/local";
 
 const kernelLocal: FileNodeLocal = {
     name: "Edgeless_Beta_4.1.0.iso",

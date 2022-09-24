05ace80af2aaa5bdfbd47ff6b9e84def363905ab
L0serj3rry <dsyourshy@qq.com>
2022-09-24 21:19:18 +0800

    feat:使用HashHistory以解决构建版本无法渲染路由视图的问题

--- src/router/history.ts
+++ src/router/history.ts
@@ -1,3 +1,3 @@
-import { createBrowserHistory } from "history";
+import { createHashHistory } from "history";
 
-export const myHistory = createBrowserHistory({ window });
+export const myHistory = createHashHistory({ window });

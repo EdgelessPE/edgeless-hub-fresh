02bb36efc2859d728fa4005bf6713dbd11b1e6f3
Cno <dongheming@bytedance.com>
2022-10-20 17:48:16 +0800

    polish:统一引号

--- electron/main/services/integrity/blake3.ts
+++ electron/main/services/integrity/blake3.ts
@@ -11,13 +11,13 @@ async function getHash(filePath: string): Promise<Result<string, string>> {
     await load()
     const stream = fs.createReadStream(filePath)
     const hash = createHash();
-    stream.on('data', (d) => hash.update(d));
-    stream.on('error', (err) => {
+    stream.on("data", (d) => hash.update(d));
+    stream.on("error", (err) => {
       hash.dispose();
       resolve(new Err(`Error:Can't calculate hash for ${filePath} : ${JSON.stringify(err)}`))
       return
     });
-    stream.on('end', () => {
+    stream.on("end", () => {
       const res = hash.digest("hex") as unknown as string
       resolve(new Ok(res))
       return
